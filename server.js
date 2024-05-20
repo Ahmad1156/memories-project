886e6ce95731f95621500a9c716126726b412c80
ahmad shakik <ahmadshakik777@gmail.com>
2024-05-21 02:06:16 +0300

    added

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './db/connectDB.js';
-import dotenv from 'dotenv';
-import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js';
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
-
-const app=express();
+const app = express();
 dotenv.config();
 
-app.use(express.json({limit:"10mb"}));
-app.use(express.urlencoded({limit:"10mb",extended:true}));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 
-app.use('/posts',postRoutes);
-app.use('/users',userRoutes);
+app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
 
+const PORT = process.env.PORT || 5000;
 
-
-
-const PORT=process.env.PORT || 5000;
-
-const start=async ()=>{
-    try {
-      await connectDB(process.env.MONGO_URL);
-      app.listen(PORT,console.log(`server is listening on port ${PORT}..
-      `));
-    } catch(error){
-      console.log(error.message);
-    }
-}
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(
+      PORT,
+      console.log(`server is listening on port ${PORT}..
+      `)
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 start();
